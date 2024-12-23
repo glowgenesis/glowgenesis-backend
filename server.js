d6106e6d74a9d1b981a42b848b4973c74f5b25d4
glowgenesis <glowgenesisskincarepvtltd@gmail.com>
2024-12-23 21:00:14 +0530

    server.js

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use('/route', userRoutes);
 app.use('/route', loginotproute)
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
